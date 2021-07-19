9a23af64183963e9b4edba52541f2791c0eac77f
Franco Aguilar <ratiuxjara43@gmail.com>
2021-07-19 00:51:37 -0600

    FAM 19/Jul/2021

---
+++ core/static/fecha/fecha.js
@@ -0,0 +1,46 @@
+function datetimepicker_settings() {
+    $('#date_timepicker_start').datetimepicker({
+        onShow: function () {
+            this.setOptions({
+                maxDate: $('#date_timepicker_end').val() ? $('#date_timepicker_end').val() : false
+            })
+        },
+        timepicker: true
+    });
+    $('#date_timepicker_end').datetimepicker({
+        onShow: function () {
+            this.setOptions({
+                minDate: $('#date_timepicker_start').val() ? $('#date_timepicker_start').val() : false
+            })
+        },
+        timepicker: true
+    });
+};
+
+
+function seleccionar_sedes() {
+    activado = false
+
+    $('#flexCheckDefault').on('click', function () {
+        if (activado) {
+            $(".btn-check").prop('disabled', false);
+            $(".btn-check").prop('checked', false).parent().addClass('active');
+            activado = false;
+        } else {
+            $(".btn-check").prop('disabled', true);
+            $(".btn-check").prop('checked', true).parent().addClass('active');
+            activado = true
+        }
+    });
+}
+
+function obtener_sedes() {
+    var sedes_seleccinadas;
+    $("form").submit(function (event) {
+        event.preventDefault();
+        sedes_seleccinadas = $('.sedes-btn:checkbox:checked');
+        console.log(sedes_seleccinadas)
+    });
+    return sedes_seleccinadas;
+}
+
